21e71eb4d33d80755504030ca79834f75ff87f44
Justin Hornosty (jjrh) <justin@credil.org>
2015-03-11 14:34:58 -0400

    added the midishield for 3.3v

---
+++ midishield-3_3V.cmd
@@ -0,0 +1,58 @@
+# Pin name action command file
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element CONN3
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 3, 3)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element 1
+ChangePinName(1, 2, 2)
+ChangePinName(1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 6, Vcc)
+ChangePinName(U1, 5, GND)
+ChangePinName(U1, 4, 4)
+ChangePinName(U1, 1, 1)
+ChangePinName(U1, 2, 2)
+
+# Start of element MIDI_OUT1
+ChangePinName(MIDI_OUT1, Shield, Shield)
+ChangePinName(MIDI_OUT1, 5, 5)
+ChangePinName(MIDI_OUT1, 4, 4)
+ChangePinName(MIDI_OUT1, 3, 3)
+ChangePinName(MIDI_OUT1, 2, 2)
+ChangePinName(MIDI_OUT1, 1, 1)
+
+# Start of element MIDI_IN1
+ChangePinName(MIDI_IN1, Shield, Shield)
+ChangePinName(MIDI_IN1, 5, 5)
+ChangePinName(MIDI_IN1, 4, 4)
+ChangePinName(MIDI_IN1, 3, 3)
+ChangePinName(MIDI_IN1, 2, 2)
+ChangePinName(MIDI_IN1, 1, 1)
